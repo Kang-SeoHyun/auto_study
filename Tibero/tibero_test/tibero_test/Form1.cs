3509008efe447ea75814edadd409fb7452b01813
Kang-SeoHyun <kang061020@gmail.com>
2023-06-16 10:11:31 +0900

    Refactor: tibero_test

--- Tibero/tibero_test/tibero_test/Form1.cs
+++ Tibero/tibero_test/tibero_test/Form1.cs
@@ -16,5 +16,10 @@ namespace tibero_test
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("hello", "가입완료");
+        }
     }
 }
